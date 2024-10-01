fbb3f60c60ff8b5f4475073be4f7aa1741c223ae
ricardosoriagalvarroguerra <110310949+ricardosoriagalvarroguerra@users.noreply.github.com>
2024-10-01 02:56:17 +0000

    modified:   BOL.py modified:   requirements.txt

--- BOL.py
+++ BOL.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 # Configuración de la página
 st.set_page_config(
@@ -7,8 +8,29 @@ st.set_page_config(
     layout="wide"
 )
 
-# Título principal centrado
-st.markdown(
-    "<h1 style='text-align: center;'>Overview de la Economía de Bolivia</h1>",
-    unsafe_allow_html=True
+# Barra de navegación con option_menu
+page = option_menu(
+    menu_title=None,  # No mostrar el título del menú
+    options=["Overview", "Indicadores Clave", "Comercio Exterior", "Coyuntura Laboral"],
+    icons=["house", "bar-chart", "briefcase", "clipboard"],  # Puedes cambiar los iconos
+    menu_icon="cast", 
+    default_index=0, 
+    orientation="horizontal"
 )
+
+# Contenido de las páginas
+if page == "Overview":
+    st.markdown("<h3 style='text-align: left; margin-top: -50px;'>Overview</h3>", unsafe_allow_html=True)
+    st.write("Contenido de la página Overview")
+
+elif page == "Indicadores Clave":
+    st.title("Indicadores Clave")
+    st.write("Contenido de Indicadores Clave")
+
+elif page == "Comercio Exterior":
+    st.title("Comercio Exterior")
+    st.write("Contenido de Comercio Exterior")
+
+elif page == "Coyuntura Laboral":
+    st.title("Coyuntura Laboral")
+    st.write("Contenido de Coyuntura Laboral")
